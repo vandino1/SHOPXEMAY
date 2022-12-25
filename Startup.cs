a6a6cff89c1f237309ef74f53e216e83ff9932ed
vandino1 <79361911+vandino1@users.noreply.github.com>
2022-12-25 18:25:29 +0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,100 @@
+﻿using HondaVN.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HondaVN
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)// nếu là true phải đợi xác nhận email mới cho đăng nhập
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders()
+                .AddDefaultUI();
+            
+            services.AddAuthentication()
+    .AddGoogle(googleOptions =>
+    { // Read Authentication:Google information from appsettings.json 
+        IConfigurationSection googleAuthNSection = Configuration.GetSection("Authentication:Google"); // Set up ClientID and ClientSecret to access google API 
+        googleOptions.ClientId = googleAuthNSection["ClientId"];
+        googleOptions.ClientSecret = googleAuthNSection["ClientSecret"];
+     
+    }).AddFacebook(facebookOptions => {
+        // Đọc cấu hình
+        IConfigurationSection facebookAuthNSection = Configuration.GetSection("Authentication:Facebook");
+        facebookOptions.AppId = facebookAuthNSection["AppId"];
+        facebookOptions.AppSecret = facebookAuthNSection["AppSecret"];
+        // Thiết lập đường dẫn Facebook chuyển hướng đến
+        facebookOptions.CallbackPath = "/signin-facebook";
+    });
+
+
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>      
+            {
+                options.Cookie.Name = "HondaVN";
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+
+            });
+        }
+ 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseSession();
+
+            app.UseRouting();
+
+            app.UseAuthentication();// Phục hồi thông tin đăng nhập (xác thực)
+            app.UseAuthorization();// Phục hồi thông tinn về quyền của User
+
+            //Role-based authorization - xác thực quyền theo vai trò.
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
